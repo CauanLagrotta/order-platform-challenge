1e9a658faf185809b58d74a5282d09c30af03454
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-04-16 22:31:23 -0300

    feat(product): add product service and controller

--- src/main/java/com/cauanlagrotta/order_platform_challange/controller/ProductController.java
+++ src/main/java/com/cauanlagrotta/order_platform_challange/controller/ProductController.java
@@ -1,4 +1,26 @@
 package com.cauanlagrotta.order_platform_challange.controller;
 
+import com.cauanlagrotta.order_platform_challange.dto.ProductRequestDTO;
+import com.cauanlagrotta.order_platform_challange.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> create(@Valid @RequestBody ProductRequestDTO dto){
+    productService.create(dto);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/com/cauanlagrotta/order_platform_challange/services/ProductService.java
+++ src/main/java/com/cauanlagrotta/order_platform_challange/services/ProductService.java
@@ -1,4 +1,19 @@
 package com.cauanlagrotta.order_platform_challange.services;
 
+import com.cauanlagrotta.order_platform_challange.dto.ProductRequestDTO;
+import com.cauanlagrotta.order_platform_challange.entity.Product;
+import com.cauanlagrotta.order_platform_challange.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public Product create(ProductRequestDTO dto){
+    return productRepository.save(dto.toProduct());
+  }
 }
