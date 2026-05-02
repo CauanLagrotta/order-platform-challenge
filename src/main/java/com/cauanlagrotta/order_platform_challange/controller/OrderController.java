33e28cc9583ebc5b3cbff57d427ed3b65613c1ec
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-05-02 12:36:43 -0300

    feat(order): add GET /orders endpoint to list customer orders with pagination

--- src/main/java/com/cauanlagrotta/order_platform_challange/controller/OrderController.java
+++ src/main/java/com/cauanlagrotta/order_platform_challange/controller/OrderController.java
@@ -1,12 +1,17 @@
 package com.cauanlagrotta.order_platform_challange.controller;
 
+import com.cauanlagrotta.order_platform_challange.dto.ListResponse;
 import com.cauanlagrotta.order_platform_challange.dto.OrderRequestDTO;
 import com.cauanlagrotta.order_platform_challange.dto.OrderResponseDTO;
+import com.cauanlagrotta.order_platform_challange.dto.PaginationResponse;
 import com.cauanlagrotta.order_platform_challange.services.OrderService;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -26,5 +31,20 @@ public class OrderController {
   @GetMapping("/{id}")
   public ResponseEntity<OrderResponseDTO> findById(@PathVariable UUID id){
     return ResponseEntity.ok(orderService.findById(id));
+  }
+
+  @GetMapping
+  public ResponseEntity<ListResponse<OrderResponseDTO>>
+      listCustomersOrders(@RequestParam("customerId") UUID customerId,
+                          @RequestParam(name = "page", defaultValue = "0") Integer page,
+                          @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
+
+    var pageResponse = orderService.findAllByCustomerId(PageRequest.of(page, pageSize), customerId);
+
+    return ResponseEntity.ok(new ListResponse<>(
+        Map.of("totalOrders", pageResponse.getTotalElements()),
+        pageResponse.getContent(),
+        PaginationResponse.fromPage(pageResponse)
+    ));
   }
 }
