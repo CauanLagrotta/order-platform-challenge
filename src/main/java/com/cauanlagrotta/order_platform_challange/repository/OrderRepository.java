ca7c617dbedaa04fbc4521fa36ed7b66da6fac08
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-08-31 23:56:26 -0300

    fix(order): add EntityGraph to eagerly load associations in queries

--- src/main/java/com/cauanlagrotta/order_platform_challange/repository/OrderRepository.java
+++ src/main/java/com/cauanlagrotta/order_platform_challange/repository/OrderRepository.java
@@ -3,10 +3,12 @@ package com.cauanlagrotta.order_platform_challange.repository;
 import com.cauanlagrotta.order_platform_challange.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+  @EntityGraph(attributePaths = {"product", "customer"})
   Page<Order> findByCustomerId_Id(UUID customerId, PageRequest pageRequest);
 }
